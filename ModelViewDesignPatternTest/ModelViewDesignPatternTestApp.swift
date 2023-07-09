32748098d074aa6a9c735bbbbfca993b79038970
David Malicke <davemalicke@gmail.com>
2023-07-08 21:18:56 -0400

    Initial Commit

---
+++ ModelViewDesignPatternTest/ModelViewDesignPatternTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModelViewDesignPatternTestApp.swift
+//  ModelViewDesignPatternTest
+//
+//  Created by David Malicke on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct ModelViewDesignPatternTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
